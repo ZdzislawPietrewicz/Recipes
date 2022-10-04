de43b775d95b68358fa9c7b845ee91319b4c16c7
Zdzislaw Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2022-10-04 21:07:12 +0200

    Rest controller modification - 2 methods invoke methods from RecipeService

--- recipes/controller/Controller.java
+++ recipes/controller/Controller.java
@@ -1,9 +1,12 @@
 package recipes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import recipes.model.Recipe;
+import recipes.persistance.RecipeRepository;
+import recipes.service.RecipeService;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -11,22 +14,22 @@ import java.util.Map;
 
 @RestController
 public class Controller {
-    private Map<Integer, Recipe> recipeMap = new HashMap<>();
+    @Autowired
+    RecipeService recipeService;
 
     @GetMapping("/api/recipe/{id}")
-    public Recipe getRecipe(@PathVariable int id) {
-        Recipe recipe = recipeMap.get(id);
+    public Recipe getRecipe(@PathVariable Long id) {
+        Recipe recipe = recipeService.getRecipe(id);
         if (recipe == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,
                     "Recipe of given id not found");
         }
-        return recipeMap.get(id);
+        return recipe;
     }
 
     @PostMapping("/api/recipe/new")
-    public Map<String, Integer> addRecipe(@RequestBody Recipe recipe) {
-        int id = recipeMap.size() + 1;
-        recipeMap.put(id, recipe);
-        return Collections.singletonMap("id", id);
+    public Map<String, Long> saveRecipe(@RequestBody Recipe recipe) {
+        recipeService.saveRecipeToDB(recipe);
+        return Collections.singletonMap("id", recipe.getId());
     }
 }
