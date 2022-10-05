5e981ab07353a27bd7500a9672a04d25c1838b8a
Zdzislaw Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2022-10-05 12:44:33 +0200

    changed annotation in model class, fields name and description can't be empty, Json arrays have minimum 1 element

--- recipes/model/Recipe.java
+++ recipes/model/Recipe.java
@@ -1,14 +1,31 @@
 package recipes.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @NotBlank
     private String description;
-    private String ingredients;
-    private String directions;
+
+    @Size(min = 1)
+    private String [] ingredients;
+
+    @Size(min = 1)
+    private String [] directions;
 
 }
