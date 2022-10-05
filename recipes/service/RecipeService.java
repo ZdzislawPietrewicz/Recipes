c7502748e87dc18964770442384cb9848c1a8c9b
Zdzislaw Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2022-10-05 14:05:28 +0200

    delete recipe from database method improved

--- recipes/service/RecipeService.java
+++ recipes/service/RecipeService.java
@@ -2,6 +2,9 @@ package recipes.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import recipes.errors.RecipeDoesNotExist;
 import recipes.model.Recipe;
 import recipes.persistance.RecipeRepository;
 
@@ -15,9 +18,20 @@ public class RecipeService {
     public Recipe saveRecipeToDB(Recipe recipe) {
         return recipeRepository.save(recipe);
     }
-    public Recipe getRecipe(Long id) {
-        Optional<Recipe>foundRecipe= recipeRepository.findById(id);
-        if(foundRecipe.isPresent()) return foundRecipe.get();
-        else return null;
+
+    public Optional<Recipe> getRecipe(Long id) {
+        Optional<Recipe> foundRecipe = recipeRepository.findById(id);
+        return foundRecipe;
+    }
+
+    public ResponseEntity<String> deleteRecipe(Long id) {
+        Optional<Recipe> recipeToDelete = getRecipe(id);
+        recipeToDelete.ifPresentOrElse((recipe) -> {
+                    recipeRepository.delete(recipe);
+                },
+                () -> {
+                    throw new RecipeDoesNotExist("Recipe with this id does not exist");
+                });
+        return new ResponseEntity<>("Recipe with given id deleted",HttpStatus.NO_CONTENT);
     }
 }
