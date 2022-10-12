3c21d2bce9dd5f214b98391e91b54b00d961c498
Zdzislaw Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2022-10-12 16:34:27 +0200

    new endpoints added

--- recipes/controller/Controller.java
+++ recipes/controller/Controller.java
@@ -6,21 +6,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import recipes.model.Recipe;
-import recipes.persistance.RecipeRepository;
 import recipes.service.RecipeService;
 
 import javax.validation.Valid;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/recipe")
 public class Controller {
     @Autowired
     RecipeService recipeService;
 
-    @GetMapping("/api/recipe/{id}")
+    @GetMapping("/{id}")
     public Recipe getRecipe(@PathVariable Long id) {
         Optional<Recipe> recipe = recipeService.getRecipe(id);
         if (recipe.isEmpty()) {
@@ -30,13 +30,27 @@ public class Controller {
         return recipe.get();
     }
 
-    @PostMapping("/api/recipe/new")
+    @PostMapping("/new")
     public Map<String, Long> saveRecipe(@Valid @RequestBody Recipe recipe) {
         recipeService.saveRecipeToDB(recipe);
         return Collections.singletonMap("id", recipe.getId());
     }
-    @DeleteMapping("/api/recipe/{id}")
+
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteRecipe(@PathVariable Long id) {
         return recipeService.deleteRecipe(id);
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateRecipe(@Valid @RequestBody Recipe recipe, @PathVariable Long id){
+        return recipeService.updateRecipe(recipe, id);
+    }
+    @GetMapping(value = "/search", params = "category")
+    public List<Recipe> getRecipesByCategory (@RequestParam String category){
+        return recipeService.findRecipeByCategoryIngoreCaseOrderByDateDesc(category);
+    }
+    @GetMapping(value = "/search", params = "name")
+    public List<Recipe> getRecipesByName (@RequestParam String name){
+        return  recipeService.findRecipeByNameContainingIgnoreCaseOrderByDateDesc(name);
+    }
+
 }
