eeb15aa1485134cb1576ea733d85d99049111fef
Zdzislaw Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2022-10-12 16:36:35 +0200

    methods to search recipes according to criteria

--- recipes/persistance/RecipeRepository.java
+++ recipes/persistance/RecipeRepository.java
@@ -1,9 +1,20 @@
 package recipes.persistance;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import recipes.model.Recipe;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
-public interface RecipeRepository extends CrudRepository<Recipe,Long> {
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
+    List<Recipe> findRecipesByCategoryIgnoreCaseOrderByDateDesc(String category);
+
+    List<Recipe> findRecipesByNameContainsIgnoreCaseOrderByDateDesc(String name);
+
 }
