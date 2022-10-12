c6a1dc9f176583499c114d1461a9293e1127a98f
Zdzislaw Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2022-10-12 16:37:21 +0200

    methods to search recipes and update them

--- recipes/service/RecipeService.java
+++ recipes/service/RecipeService.java
@@ -5,10 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import recipes.errors.RecipeDoesNotExist;
+import recipes.errors.SearchParamNotValid;
 import recipes.model.Recipe;
 import recipes.persistance.RecipeRepository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Configuration
 public class RecipeService {
@@ -32,6 +37,30 @@ public class RecipeService {
                 () -> {
                     throw new RecipeDoesNotExist("Recipe with this id does not exist");
                 });
-        return new ResponseEntity<>("Recipe with given id deleted",HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>("Recipe with given id deleted", HttpStatus.NO_CONTENT); // 204 no content
     }
+
+    public ResponseEntity<String> updateRecipe(Recipe recipe, Long id) {
+        Optional<Recipe> recipeToUpdate = getRecipe(id);
+        recipeToUpdate.ifPresentOrElse((rec) -> {
+            rec.setName(recipe.getName());
+            rec.setDescription(recipe.getDescription());
+            rec.setCategory(recipe.getCategory());
+            rec.setIngredients(recipe.getIngredients());
+            rec.setDirections(recipe.getDirections());
+            recipeRepository.save(rec);
+        }, () -> {
+            throw new RecipeDoesNotExist("Recipe with this id does not exist");
+        });
+        return new ResponseEntity<>("Recipe given with id is updated", HttpStatus.NO_CONTENT); // 204
+    }
+
+    public List<Recipe> findRecipeByCategoryIngoreCaseOrderByDateDesc(String category) {
+        return recipeRepository.findRecipesByCategoryIgnoreCaseOrderByDateDesc(category);
+    }
+
+    public List<Recipe> findRecipeByNameContainingIgnoreCaseOrderByDateDesc(String name) {
+        return recipeRepository.findRecipesByNameContainsIgnoreCaseOrderByDateDesc(name);
+    }
+
 }
